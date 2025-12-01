589eeaf1dfc5589a82ab27cf5f55b646c5aff69c
jon tarrant <jon.tarrant@hotmail.co.uk>
2025-12-01 22:07:09 +0100

    Update embedded YouTube video to gE7srp2BzoM

--- pages/what-is-scrum-poker.tsx
+++ pages/what-is-scrum-poker.tsx
@@ -36,7 +36,7 @@ export default function WhatIsScrumPoker() {
             <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
               <iframe
                 className="absolute top-0 left-0 w-full h-full rounded-lg"
-                src="https://www.youtube.com/embed/MZoO8-Q46vY"
+                src="https://www.youtube.com/embed/gE7srp2BzoM"
                 title="Scrum Poker / Planning Poker Tutorial"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
